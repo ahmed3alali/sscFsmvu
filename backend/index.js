7dd1254ffc3edd0154150eef67560a4791abd7b7
ahmed3alali <ahmed3alali.development@gmail.com>
2025-03-28 19:04:28 +0300

    final test for today

--- backend/index.js
+++ backend/index.js
@@ -6,48 +6,45 @@ import dotenv from 'dotenv';
 import announcementRouter from "../backend/Router/AnnouncementRouter.js"
 import userRoutes from "../backend/Router/authRoutes.js"
 import photoRouter from "../backend/Router/ImageRouter.js"
-
 const app = express();
-
-// CORS middleware: Ensure it is placed before any routes are handled
 app.use(cors({
-  origin: 'https://ssc-fsmvu.vercel.app',  // Replace with your frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
+    origin: '*',  // replace with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],  // Specify the allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials:true,  // Specify the allowed headers
+  }));
 
-// Alternatively, if you're using the custom middleware, you can add that instead:
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://ssc-fsmvu.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+  app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
 
-// Handle uncaught exceptions
+
+  // Handle uncaught exceptions
 process.on('uncaughtException', (err) => {
     console.log(`ERROR : ${err}`);
     console.log("Shutting down due to uncaught exception");
     process.exit(1);
 });
 
-if (process.env.NODE_ENV !== 'PRODUCTION') {
+if (process.env.NODE_ENV!=='PRODUCTION') {
     dotenv.config();
+    
 }
 
+
 const MongoUrl = process.env.DB_LOCAL_URI;
-mongoose.connect(MongoUrl).then(() => {
-    console.log("Connected to SSC FSMVU Database Successfully!");
-}).catch((error) => {
-    console.log(error);
-});
+mongoose.connect(MongoUrl).then(()=>{
 
-app.use(express.json({ limit: '10mb' }));
-app.use("/api/v1", announcementRouter);
-app.use("/api/v1", userRoutes);
-app.use("/api/v1", photoRouter);
+    console.log("connected to SSC FSMVU Database Successfully  !");
+}).catch((error)=>{
 
+console.log(error);
+
+})
+
+app.use(express.json({limit:'10mb'}));
+app.use("/api/v1",announcementRouter);
+app.use("/api/v1",userRoutes);
+app.use("/api/v1",photoRouter);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
